1b8775bb6ff595ec6e70f7db839e1c3604495f90
Gopikrishnan.p <95020873+Gopikrishnan3007@users.noreply.github.com>
2024-03-12 10:00:18 +0530

    Update Sample1.java

--- Sample1.java
+++ Sample1.java
@@ -1,7 +1,5 @@
 Class Sample1{
   public static void main(String args[]){
     System.out.println("The 2nd file named Sample1");
-    System.out.println("This line added in branch1");
-    System.out.println("This line added in branch1");
   }
 }
